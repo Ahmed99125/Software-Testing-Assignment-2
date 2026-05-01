0efb0870b7d4200c10fa772152bbb8775e4dfa98
George Raafat <georgexraafat@gmail.com>
2026-05-01 09:43:52 +0300

    add dependencies

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         String driverPath = "src/test/resources/drivers/chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", driverPath);
 
